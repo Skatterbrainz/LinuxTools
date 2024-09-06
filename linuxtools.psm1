0b14705f12adcc2fbd902df40e945c1124ee6b11
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2024-09-06 01:13:44 -0400

    1.0.5

--- linuxtools.psm1
+++ linuxtools.psm1
@@ -2,7 +2,10 @@
 #
 
 # Import functions within module
-write-host "Importing functions from LinuxTools module: $($PSScriptRoot)"
+#write-host "Importing functions from LinuxTools module: $($PSScriptRoot)"
+if (-not $IsLinux) {
+	throw "This module is only supported on Linux"
+}
 ('private','public') | Foreach-Object {
 	Get-ChildItem -Path $(Join-Path -Path $PSScriptRoot -ChildPath $_) -Filter "*.ps1" | Foreach-Object { write-verbose $_.FullName; . $_.FullName }
 }
