5ed689b7d396d2cf27e6b78fd24b8b95cad8ebe0
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2024-09-20 00:54:13 -0400

    1.0.8 - init

--- public/Get-AppLaunchers.ps1
+++ public/Get-AppLaunchers.ps1
@@ -4,13 +4,36 @@ function Get-AppLaunchers {
 		Get the application launchers on the system.
 	.DESCRIPTION
 		This function gets the application launchers on the system.
+	.PARAMETER Scope
+		The scope of the application launchers to get. The default is "User".
+	.PARAMETER Name
+		The name of the application launcher to get. If not specified, all application launchers are returned.
+	.PARAMETER Contents
+		If Name is provided, this returns the contents of the matching application launchers.
 	.EXAMPLE
 		Get-AppLaunchers
+
+		Get all application launchers on the system for the current user.
+	.EXAMPLE
+		Get-AppLaunchers -Scope System
+
+		Get all application launchers on the system for all users.
+	.EXAMPLE
+		Get-AppLaunchers -Name Firefox
+
+		Get the application launcher for Firefox for the current user.
+	.EXAMPLE
+		Get-AppLaunchers -Name Firefox -Contents
+
+		Get the contents of the application launcher for Firefox for the current user.
 	.LINK
+		https://github.com/Skatterbrainz/linuxtools/blob/master/docs/Get-AppLaunchers.md
 	#>
 	[CmdletBinding()]
 	param (
-		[parameter()][string][ValidateSet("User","System")]$Scope = "User"
+		[parameter()][string][ValidateSet("User","System")]$Scope = "User",
+		[parameter()][string]$Name,
+		[parameter()][switch]$Contents
 	)
 	try {
 		$UserPath = "~/.local/share/applications"
@@ -19,9 +42,15 @@ function Get-AppLaunchers {
 		if (!(Test-Path -Path $AppPath)) {
 			throw "Path not found: $AppPath"
 		}
-		$launchers = Get-ChildItem -Path $AppPath -Filter "*.desktop" -Recurse -ErrorAction Stop
-		foreach ($launcher in $launchers) {
-			[pscustomobject]$(Get-IniContent -FilePath $launcher.FullName).Values
+		$launchers = Get-ChildItem -Path $AppPath -Filter "*.desktop" -File -ErrorAction Stop
+		if (![string]::IsNullOrEmpty($Name)) {
+			if ($Contents.IsPresent) {
+				$launchers | Where-Object { $_.BaseName -like "*$Name*" } | ForEach-Object { Get-Content -Path $_.FullName }
+			} else {
+				$launchers | Where-Object { $_.BaseName -like "*$Name*" } | Select-Object -Property BaseName, FullName -Unique
+			}
+		} else {
+			$launchers | Select-Object -Property BaseName, FullName | Sort-Object BaseName -Unique
 		}
 	} catch {
 		Write-Error "Failed to get application launchers: $($_.Exception.Message)"
