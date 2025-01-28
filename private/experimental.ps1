2a1e47ebd9f5dca04f72ebaba4f98136a222bfc5
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2025-01-28 01:51:20 -0500

    1.0.18

---
+++ private/experimental.ps1
@@ -0,0 +1,225 @@
+<#
+Win32 CIM/WMI Provider classes
+
+Win32_Fan
+Win32_HeatPipe
+Win32_Refrigeration
+Win32_TemperatureProbe
+
+Win32_Keyboard
+Win32_PointingDevice
+
+Win32_PortableBattery
+Win32_UninterruptiblePowerSupply
+Win32_AutochkSetting
+Win32_ComputerSystem
+Win32_ComputerSystemProcessor
+Win32_ComputerSystemProduct
+Win32_SystemEnclosure
+Win32_OperatingSystem
+	BootDevice
+	BuildNumber
+	BuildType
+	Caption
+	CodeSet
+	CountryCode
+	CreationClassName
+	CSCreationClassName
+	CSDVersion
+	CSName
+	CurrentTimeZone
+	DataExecutionPrevention_Available
+	DataExecutionPrevention_32BitApplications
+	DataExecutionPrevention_Drivers
+	DataExecutionPrevention_SupportPolicy
+	Debug
+	Description
+	Distributed
+	EncryptionLevel
+	ForegroundApplicationBoost
+	FreePhysicalMemory
+	FreeSpaceInPagingFiles
+	FreeVirtualMemory
+	InstallDate
+	LargeSystemCache
+	LastBootUpTime
+	LocalDateTime
+	Locale
+	Manufacturer
+	MaxNumberOfProcesses
+	MaxProcessMemorySize
+	MUILanguages[]
+	Name
+	NumberOfLicensedUsers
+	NumberOfProcesses
+Win32_LogicalDisk
+	Access
+	Availability
+	BlockSize
+	Caption
+	Compressed
+	ConfigManagerErrorCode
+	ConfigManagerUserConfig
+	CreationClassName
+	Description
+	DeviceID
+	DriveType
+	ErrorCleared
+	ErrorDescription
+	ErrorMethodology
+	FileSystem
+	FreeSpace
+	InstallDate
+	LastErrorCode
+	MaximumComponentLength
+	MediaType
+	Name
+	NumberOfBlocks
+	PNPDeviceID
+	PowerManagementCapabilities[]
+	PowerManagementSupported
+	ProviderName
+	Purpose
+	QuotasDisabled
+	QuotasIncomplete
+	QuotasRebuilding
+	Size
+	Status
+	StatusInfo
+	SupportsDiskQuotas
+	SupportsFileBasedCompression
+	SystemCreationClassName
+	SystemName
+	VolumeDirty
+	VolumeName
+	VolumeSerialNumber
+Win32_LogicalDiskToPartition
+Win32_DiskDrive
+Win32_CDROMDrive
+Win32_FloppyDrive
+Win32_PhysicalMedia
+Win32_TapeDrive
+
+# Motherboard, Controller and Port Classes
+
+Win32_1394Controller
+Win32_1394ControllerDevice
+Win32_AllocatedResource
+Win32_AssociatedProcessorMemory
+Win32_BaseBoard
+Win32_Battery
+Win32_Bus
+Win32_BIOS
+	BiosCharacteristics[]
+	BiosVersion[]
+	BuildNumber
+	Caption
+	CodeSet
+	CurrentLanguage
+	Description
+	EmbeddedControllerMajorVersion
+	EmbeddedControllerMinorVersion
+	IdentificationCode
+	InstallableLanguages
+	InstallDate
+	LanguageEdition
+	ListOfLanguages[]
+	Manufacturer
+	Name
+	OtherTargetOS
+	PrimaryBIOS
+	ReleaseDate
+	SerialNumber
+	SMBIOSBIOSVersion
+	SMBIOSMajorVersion
+	SMBIOSMinorVersion
+	SMBIOSPresent
+	SoftwareElementID
+	SoftwareElementState
+	Status
+	SystemBiosMajorVersion
+	SystemBiosMinorVersion
+	TargetOperatingSystem
+	Version
+Win32_Bus
+Win32_CacheMemory
+Win32_ControllerHasHub
+Win32_DesktopMonitor
+Win32_DeviceBus
+Win32_DeviceMemoryAddress
+Win32_DeviceSettings
+Win32_DMAChannel
+Win32_IDEController
+Win32_IDEControllerDevice
+Win32_InfraredDevice
+Win32_IRQResource
+Win32_MemoryArray
+Win32_MemoryArrayLocation
+Win32_MemoryDevice
+Win32_MemoryDeviceArray
+Win32_MemoryDeviceLocation
+Win32_MotherboardDevice
+Win32_OnBoardDevice
+Win32_PCMCIAController
+Win32_ParallelPort
+Win32_PhysicalMemory
+Win32_PhysicalMemoryArray
+Win32_PhysicalMemoryLocation
+Win32_PNPAllocatedResource
+Win32_PnPDevice
+Win32_PnPEntity
+Win32_PortConnector
+Win32_PortResource
+Win32_Processor
+Win32_SCSIController
+Win32_SCSIControllerDevice
+Win32_SerialPort
+Win32_SerialPortConfiguration
+Win32_SerialPortSetting
+Win32_SMBIOSMemory
+Win32_SoundDevice
+Win32_SystemBIOS
+Win32_SystemDriverPNPEntity
+Win32_SystemEnclosure
+Win32_SystemMemoryResource
+Win32_SystemSlot
+Win32_USBController
+Win32_USBControllerDevice
+Win32_USBHub
+
+# Network Classes
+
+Win32_NetworkAdapter
+Win32_NetworkAdapterConfiguration
+Win32_NetworkAdapterSetting
+
+# Power Management Classes
+
+Win32_Battery
+Win32_CurrentProbe
+Win32_PortableBattery
+Win32_PowerManagementEvent
+Win32_VoltageProbe
+
+# Printing Classes
+
+Win32_Printer
+Win32_PrinterConfiguration
+Win32_PrinterController
+Win32_PrinterDriver
+Win32_PrinterDriverDll
+Win32_PrinterSetting
+Win32_PrintJob
+Win32_TCPIPPrinterPort
+
+# Telephony Classes
+
+Win32_POTSModem
+Win32_POTSModemToSerialPort
+
+# Video and Monitor Classes
+Win32_DesktopMonitor
+Win32_DisplayConfiguration
+Win32_VideoController
+Win32_VideoSettings
+#>
