9d2ef06baa71bcd567e08208e316a97957af0c0f
skatterbrainz <dstein2017@gmail.com>
2025-04-29 13:55:23 -0400

    1.1.0

--- public/Get-LinuxDistro.ps1
+++ public/Get-LinuxDistro.ps1
@@ -6,6 +6,8 @@ function Get-LinuxDistro {
 		This function reads the /etc/os-release file to determine the Linux distribution name.
 	.PARAMETER distro
 		The Linux distribution name. If not provided, the function will attempt to determine it.
+	.PARAMETER Detailed
+		Include PSVersionTable OS value with output. This is appended following a semi-colon
 	.EXAMPLE
 		Get-LinuxDistro
 		Returns the Linux distribution name.
@@ -13,7 +15,8 @@ function Get-LinuxDistro {
 		https://github.com/Skatterbrainz/linuxtools/blob/master/docs/Get-LinuxDistro.md
 	#>
 	param (
-		[parameter()][string]$distro = ""
+		[parameter()][string]$distro = "",
+		[parameter()][switch]$Detailed
 	)
 	if (Test-Path "/etc/os-release") {
 		$osRelease = Get-Content -Path "/etc/os-release" -Raw	
@@ -28,6 +31,11 @@ function Get-LinuxDistro {
 		# Check for Red Hat-based distributions
 		elseif ($osRelease -match "redhat") {
 			$distro = "Red Hat"
+		}
+		if (![string]::IsNullOrEmpty($distro)) {
+			if ($Detailed.IsPresent) {
+				$distro += "; $($PSVersionTable.OS)"
+			}
 		}
 	}
 	return $distro
