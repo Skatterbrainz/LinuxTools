f9c66023f078a12e2d8a436d224c97114fdd476b
skatterbrainz <dstein2017@gmail.com>
2025-09-27 22:38:28 -0400

    1.1.5

--- public/Get-FirewallStatus.ps1
+++ public/Get-FirewallStatus.ps1
@@ -11,17 +11,37 @@ function Get-FirewallStatus {
 	.LINK
 		https://github.com/Skatterbrainz/linuxtools/blob/master/docs/Get-FirewallStatus.md
 	#>
-	$result = @{}
-	$line1 = Invoke-Command -ScriptBlock { sudo ufw status } -ErrorAction Stop
-	$result.Add("Status", $($line1.Split(":")[1].Trim()))
-	$line2 = Invoke-Command -ScriptBlock { sudo ufw version } -ErrorAction Stop
-	foreach ($line in $line2) {
-		if ($line -match "ufw") {
-			$result.Add("Version", $($line.Split(" ")[1].Trim()))
+	param (
+		[parameter(Mandatory = $false)][switch]$Detailed
+	)
+	if ($Detailed.IsPresent) {
+		$lines = Invoke-Command -ScriptBlock { sudo ufw status } -ErrorAction Stop |
+			Where-Object {$_ -notlike "Status*" -and $_ -notlike 'To*' -and $_ -notlike '--*' -and ![string]::IsNullOrEmpty($_)}
+
+		foreach ($line in $lines) {
+			$port   = $line.Substring(0, 27).Trim()
+			$action = $line.Substring(27, 11).Trim()
+			$from   = $line.Substring(38).Trim()
+			[pscustomobject]@{
+				Port   = $port
+				Action = $action
+				From   = $from
+			}
 		}
-		if ($line -match "Copyright") {
-			$result.Add("Copyright", $line)
+		return
+	} else {
+		$result = @{}
+		$line1 = Invoke-Command -ScriptBlock { sudo ufw status } -ErrorAction Stop
+		$result.Add("Status", $($line1.Split(":")[1].Trim()))
+		$line2 = Invoke-Command -ScriptBlock { sudo ufw version } -ErrorAction Stop
+		foreach ($line in $line2) {
+			if ($line -match "ufw") {
+				$result.Add("Version", $($line.Split(" ")[1].Trim()))
+			}
+			if ($line -match "Copyright") {
+				$result.Add("Copyright", $line)
+			}
 		}
+		[pscustomobject]$result
 	}
-	[pscustomobject]$result
 }
