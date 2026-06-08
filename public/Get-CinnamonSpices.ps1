2b5c7c17bfdc52d9154a779b8a81711a6115e314
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2026-06-08 00:21:46 -0400

    Refactor Get-CinnamonSpices function to streamline spice retrieval and enhance parameter handling

--- public/Get-CinnamonSpices.ps1
+++ public/Get-CinnamonSpices.ps1
@@ -3,12 +3,17 @@ function Get-CinnamonSpices {
 	.SYNOPSIS
 		Retrieves a list of installed Cinnamon spices
 	.DESCRIPTION
-		Retrieves a list of installed Cinnamon spices from the user's home directory
+		Retrieves installed Cinnamon spices, or queries Cinnamon applets and extensions through a unified interface.
+	.PARAMETER Type
+		Component type to return. Valid values are "spices", "applets", "extensions" or "all".
 	.PARAMETER Name
 		Name of the spice to retrieve details for.
 	.EXAMPLE
 		Get-CinnamonSpices
 		Retrieves a list of installed Cinnamon spices
+	.EXAMPLE
+		Get-CinnamonSpices -Type applets
+		Retrieves installed Cinnamon applets
 	.EXAMPLE
 		Get-CinnamonSpices -Name "Cinnamon-Spices-Applet"
 		Retrieves details for the specified spice
@@ -17,21 +22,8 @@ function Get-CinnamonSpices {
 	#>
 	[CmdletBinding()]
 	param(
+		[parameter()][ValidateSet('spices','applets','extensions','all')][string]$Type = 'spices',
 		[parameter()][string]$Name
 	)
-	if ([string]::IsNullOrEmpty($Name)) {
-		Get-ChildItem -Path "~/.config/cinnamon/spices" -Directory |
-			Select-Object -Property Name, @{n='ShortName';e={$_.Name.Split('@')[0]}}, FullName
-	} else {
-		Get-ChildItem -Path '~/.config/cinnamon/spices' -Directory -Filter "$($Title)*" |
-			Foreach-Object {
-				$spiceName = $_.BaseName
-				Get-ChildItem -Path $_.FullName -File -Filter '*.json' |
-					ForEach-Object {
-						$baseName = $_.BaseName
-						Get-Content -Path $_.FullName | ConvertFrom-Json |
-							Select-Object -Property *, @{n='Name';e={$spiceName}}, @{n='ShortName';e={$baseName}}
-					}
-			}
-	}
+	ReadAllCinnamonComponents -Type $Type -Name $Name
 }
